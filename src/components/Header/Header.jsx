bd6a5198855f5f4ca2d864ef355a2e707e800ab6
Azizjon | Frontend Dev <norimboyevazizjon29@gmail.com>
2025-11-01 15:01:45 +0500

    Fix Header component - use regular anchor tags instead of NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,28 @@
-// src/components/Header/Header.jsx
 import React from "react";
 import styles from "./Header.module.css";
 import logo from "../../assets/icon/Logo.svg";
+import { NavLink , Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         {/* Logo */}
         <div className={styles.logo}>
-          <img src={logo} alt="" />
+          <img src={logo} alt="Logo" />
         </div>
 
         {/* Navigation Links */}
         <div className={styles.navContainer}>
           <ul className={styles.navLinks}>
-            <li><a href="/" className={styles.navLink}>Home</a></li>
-            <li><a href="/service" className={styles.navLink}>Service</a></li>
-            <li><a href="/company" className={styles.navLink}>Company</a></li>
-            <li><a href="/career" className={styles.navLink}>Career</a></li>
-            <li><a href="/blog" className={styles.navLink}>Blog</a></li>
-            <li><a href="/contact" className={styles.navLink}>Contact us</a></li>
-          </ul>
-          
+            <li><NavLink to="/" className={styles.navLink}>Home</NavLink></li>
+            <li><NavLink to="/service" className={styles.navLink}>Service</NavLink></li>
+            <li><NavLink to="/company" className={styles.navLink}>Company</NavLink></li>
+            <li><NavLink to="/career" className={styles.navLink}>Career</NavLink></li>
+            <li><NavLink to="/blog" className={styles.navLink}>Blog</NavLink></li>
+            <li><NavLink to="/contact" className={styles.navLink}>Contact us</NavLink></li>
+          </ul>  
+           
           {/* Clone Project Button */}
           <a href="#" className={styles.cloneBtn}>
             Clone project →
